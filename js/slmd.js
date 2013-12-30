4f5a02ccffdac67272aaa4788195dc66722701cc
Sean Mongey <sm@smongey.com>
2013-12-30 17:38:19 +0000

    initial

---
+++ js/slmd.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+  $('#slider').royalSlider({
+    arrowsNav: true,
+    loop: false,
+    keyboardNavEnabled: true,
+    controlsInside: false,
+    imageScaleMode: 'fill',
+    arrowsNavAutoHide: false,
+    autoScaleSlider: true, 
+    autoScaleSliderWidth: 930,     
+    autoScaleSliderHeight: 550,
+    controlNavigation: 'none',
+    navigateByClick: true,
+    startSlideId: 0,
+    autoPlay: true,
+    transitionType:'move',
+
+    /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+    imgWidth: 930,
+    imgHeight: 550
+  });
+
+});
