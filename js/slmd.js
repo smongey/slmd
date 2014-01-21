64867ce232bd0d867b8dbaff59456596948f42b5
Sean <sean@aokstudio.com>
2014-01-21 22:50:45 +0000

    added image and autoplayed slider

--- js/slmd.js
+++ js/slmd.js
@@ -1,25 +1,28 @@
 $(document).ready(function(){
 
+    $('#slider').royalSlider({
+        arrowsNav: true,
+        loop: false,
+        keyboardNavEnabled: true,
+        controlsInside: false,
+        imageScaleMode: 'fill',
+        arrowsNavAutoHide: false,
+        autoScaleSlider: true,
+        autoScaleSliderWidth: 930,
+        autoScaleSliderHeight: 550,
+        controlNavigation: 'none',
+        navigateByClick: true,
+        startSlideId: 0,
+        autoPlay: {
+            enabled: true,
+            pauseOnHover: true,
+            delay: 4000
+        },
+        transitionType:'move',
 
-  $('#slider').royalSlider({
-    arrowsNav: true,
-    loop: false,
-    keyboardNavEnabled: true,
-    controlsInside: false,
-    imageScaleMode: 'fill',
-    arrowsNavAutoHide: false,
-    autoScaleSlider: true, 
-    autoScaleSliderWidth: 930,     
-    autoScaleSliderHeight: 550,
-    controlNavigation: 'none',
-    navigateByClick: true,
-    startSlideId: 0,
-    autoPlay: true,
-    transitionType:'move',
-
-    /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
-    imgWidth: 930,
-    imgHeight: 550
-  });
+        /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+        imgWidth: 930,
+        imgHeight: 550
+    });
 
 });
